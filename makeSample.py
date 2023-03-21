c1a789976b43926d5ea658883d0076d71a946671
php-rstmks <php.skmtsr@gmail.com>
2023-03-20 19:33:14 -0700

    k

--- makeSample.py
+++ makeSample.py
@@ -3,15 +3,15 @@ import datetime
 import radar
 
 import random
-print(radar)
 
-source_file = open('dummy1.txt', 'a')
+# source_file = open('dummy1.txt', 'a')
+source_file = open('dummy1.txt', 'w')
 
 
 start_date = datetime.datetime(2021, 1, 1)
 stop_date = datetime.datetime(2021, 12, 31)
 
-print(type(radar.random_datetime(start_date, stop_date)))
+# print(type(radar.random_datetime(start_date, stop_date)))
 date_li = []
 
 date_li.append(str(radar.random_datetime(start_date, stop_date)))
@@ -19,10 +19,15 @@ date_li.append(str(radar.random_datetime(start_date, stop_date)))
 date_li.append(str(radar.random_datetime(start_date, stop_date)))
 date_li.append(str(radar.random_datetime(start_date, stop_date)))
 
+print(date_li)
+
 
 for i in range(100):
+
+    # 2021/03.09 02;45 のようなフォーマット
     new_date_data = ("/".join(str(radar.random_datetime(start_date, stop_date)).split("-")))[:-3]
-    # new_date_data = date_data[:-3]
+
+    # print(new_date_data)
 
     score = round(random.randint(1, 20000) / 1000) * 1000
 
@@ -30,5 +35,7 @@ for i in range(100):
 
     player_id = 'player' + str(player_id)
     print("{},{},{}".format(new_date_data, player_id, score), file=source_file)
+
+source_file.close()
 
 
